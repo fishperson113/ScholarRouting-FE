9c41ed6371b3cc4f239fe704277682566a0b0cb9
Phạm Triều Dương <phamduong1132005@gmail.com>
2025-11-09 23:07:38 +0700

    init codebase

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+import { App } from './app';
+
+const root = document.getElementById('root');
+if (!root) throw new Error('No root element found');
+
